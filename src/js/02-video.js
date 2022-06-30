c4ac584982e354bc36743d4e46355e0ed89c9165
Arkadiusz-Kruk <92742646+Arkadiusz-Kruk@users.noreply.github.com>
2022-06-30 13:53:10 +0200

    a
    a

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,36 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+import { save, load } from './storage';
 
+// variables
+const iframe = document.querySelector('iframe');
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+const player = new Player(iframe);
+let currentTime = load(LOCALSTORAGE_KEY);
+
+
+player.on('timeupdate', throttle(data => saveTime(data), 1000));
+player.setCurrentTime(currentTime).then(setTime).catch(errorSetTime);
+
+// functions
+function saveTime(data) {
+    const seconds = data.seconds;
+    save(LOCALSTORAGE_KEY, seconds);   
+}
+
+function setTime (seconds) {
+    // seconds = the actual time that the player seeked to
+    console.log(`Last time:${seconds}s`);
+}
+
+function errorSetTime (error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+}
