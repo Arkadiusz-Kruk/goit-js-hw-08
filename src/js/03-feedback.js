f6ccd7300475afffa9e1b7df891b3d401634ab47
Arkadiusz-Kruk <92742646+Arkadiusz-Kruk@users.noreply.github.com>
2022-06-30 13:53:46 +0200

    b
    b

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,49 @@
+import throttle from 'lodash.throttle';
+import { save, load, remove } from './storage';
+
+// variables
+const form = document.querySelector(".feedback-form");
+
+const LOCALSTORAGE_KEY = "feedback-form-state";
+const  { email, message } = form.elements;
+
+// addEventListeners
+form.addEventListener("input", throttle(saveChanges, 500
+));
+form.addEventListener("submit", handleSubmit);
+
+getData();
+
+// Functions
+function handleSubmit(event) {
+  event.preventDefault();
+  
+  if (email.value === "" || message.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+
+  console.log(`email: ${email.value}, message: ${message.value}`);
+  remove(LOCALSTORAGE_KEY);  
+  event.target.reset();
+}
+
+function saveChanges (event) { 
+  const iputsData = {
+    email: email.value,
+    message: message.value,
+  }; 
+  save(LOCALSTORAGE_KEY, iputsData);
+};
+
+function getData() { 
+  const storedData = load(LOCALSTORAGE_KEY);
+ 
+    if (storedData === undefined ) {
+        return
+    };
+  
+    email.value = storedData.email;
+    message.value = storedData.message;
+  
+};
 
